9fd93e6fb6084fe4c371013731893e200c565c4d
Vyacheslav Tarasov <aihal21@yandex.ru>
2023-10-09 09:45:32 +0300

    test12

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -13,7 +13,8 @@ load_dotenv()
 #     "django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$",
 # )
 
-SECRET_KEY = "django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$"
+SECRET_KEY = ("django-insecure-"
+              "cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
