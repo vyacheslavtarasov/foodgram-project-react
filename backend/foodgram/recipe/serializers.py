2fdf146e5f3ad5ee2f364536dc818a7ed8f83e12
Vyacheslav Tarasov <aihal21@yandex.ru>
2023-10-13 08:51:23 +0300

    proj rework2

--- backend/foodgram/recipe/serializers.py
+++ backend/foodgram/recipe/serializers.py
@@ -97,6 +97,24 @@ class RecipeSerializer(serializers.ModelSerializer):
             "is_in_shopping_cart",
         )
 
+    def validate(self, initial_data):
+        if initial_data["username"] == "me":
+            raise serializers.ValidationError("You must not use me as login.")
+        
+        if not self.initial_data["tags"]:
+            raise serializers.ValidationError("You must assign at least one tag")
+        
+        if len(self.initial_data["tags"]) > len(set(self.initial_data["tags"])):
+            raise serializers.ValidationError("Tags must be unique.")
+        
+        if not self.initial_data["ingredients"]:
+            raise serializers.ValidationError("You must assign at least one ingredients")
+        
+        if len(self.initial_data["ingredients"]) > len(set(self.initial_data["ingredients"])):
+            raise serializers.ValidationError("Ingredients must be unique.")
+
+        return initial_data
+
     def get_ingredients_with_amount(self, obj):
         ret = (
             RecipeIngredient.objects.select_related("recipe")
